a1045ec9e1955a0a4835eba6544cb894f4b8a861
AnshuPanwar124 <anshupcmpanwar1122@gmail.com>
2025-08-19 00:06:10 +0530

    New Files added

---
+++ Faulty Calculator.py
@@ -0,0 +1,27 @@
+inp1 = input("operator")
+inp2 = input("First number")
+inp3 = input("second number")
+
+new_input = inp2+inp1+inp3
+
+if new_input == "45*3":
+    print(555)
+
+elif new_input == "56+9":
+    print(77)
+
+elif new_input =="56/6":
+    print(4)
+
+elif inp1 == "*":
+    print(int(inp2)*int(inp3))
+
+elif inp1 == "+":
+    print(int(inp2)+int(inp3))
+
+elif inp1 == "-":
+    print(int(inp2)-int(inp3))
+    
+elif inp1 == "/":
+    print(int(inp2)/int(inp3))
+
