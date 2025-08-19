68c2c36c5cb815c38aad99ca230f62c1bbb00efe
AnshuPanwar124 <anshupcmpanwar1122@gmail.com>
2025-08-19 23:58:40 +0530

    New Files added

---
+++ Operators.py
@@ -0,0 +1,61 @@
+"""
+Operators in Python
+Arithmetic operator
+assignment operator
+comparison operator
+logical operator
+Identity operator
+membership operator
+bitwise operator
+"""
+#Arithmetic Operator
+print("5+6 is ",5+6)
+print("5-6 is ",5-6)
+print("5*6 is ",5*6)
+print("5/6 is ",5/6)
+print("15//6 is ",15//6)  
+print("5**3 is ",5**3)
+print("5%5 is ",5%5)
+
+#Assignment Operator
+x=5
+print(x)
+x+=30  #x=x+30
+print(x)
+
+#Comparison Operator
+i=8
+print(i==5)
+print(i!=5)
+print(i>=5)
+
+#logical operator
+a=True
+b=False
+print(a and b)
+print(a or b)
+
+#Identity operator
+a=True
+b=False
+print(a is not b)
+print(a is b)
+
+#membership operator
+list=[3,3,2,2,39,33,35,32]
+print(32 in list)
+print(324 in list)
+print(324 not in list)
+
+#Bitwise operator
+# 0-00
+# 1-01
+# 2-10
+# 3-11
+
+print(0 & 1)
+print(0 | 1)
+
+
+
+
---
+++ Short Hand If Else.py
@@ -0,0 +1,10 @@
+a=int(input("Enter first number\n"))
+b=int(input("Enter second number\n"))
+
+if a>b: print("a is greater than b")
+
+c=int(input("Enter first number\n"))
+d=int(input("Enter second number\n"))
+
+print("d is greater than c") if c<d else print("c is graeter than d")
+
