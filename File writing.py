3edbdf07cc3823414da3e31954029e7878545409
AnshuPanwar124 <anshupcmpanwar1122@gmail.com>
2025-08-21 21:22:16 +0530

    New Files added

---
+++ File writing.py
@@ -0,0 +1,24 @@
+f = open("anshu.txt","rt")  #read and text by default
+#print(f.readline())     #print line by line
+#print(f.readline())
+#print(f.readline())
+#content  = f.read(3)      #will read 3 character
+#print(content)
+
+#content  = f.read(3)      #will read next 3 character
+#print(content)
+
+#content  = f.read()
+
+'''for line in f:              Print whole file content
+    print(line, end="")''' 
+
+"""for line in content:        Print character by character            
+    print(line) """
+
+print(f.readlines())  #Print list of lines
+
+
+
+f.close()     
+
