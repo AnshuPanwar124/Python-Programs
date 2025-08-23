a27e67f2af6cbf88024e64105e8201c9b6df31f5
AnshuPanwar124 <anshupcmpanwar1122@gmail.com>
2025-08-23 22:58:28 +0530

    New Files added

---
+++ With Block to open python files.py
@@ -0,0 +1,9 @@
+with open("anshu.txt") as f:         # It includes f=open()and f=close() function.
+    a=f.read(4)          #print first 4 char
+    print(a)
+
+#f=open("anshu.txt")
+#print(f.readlines())    #print whole lines in list
+#print(f.readline())
+
+#f.close()
