dad3aa4f362209f9c7c7b228c8d96a637be97db2
AnshuPanwar124 <anshupcmpanwar1122@gmail.com>
2025-08-28 00:21:59 +0530

    New Files added

---
+++ Modules.py
@@ -0,0 +1,12 @@
+#For importing external modules   pip install module_name
+import random #print random numbers
+
+random_number = random.randint(0,5)
+#print(random_number)
+
+rand = random.random()
+print(rand)
+
+lst = ["Star Plus","DD1", "Aajtak", "CodeWithHarry"]
+choice = random.choice(lst)
+print(choice)
