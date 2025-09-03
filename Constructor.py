1038204166fbe8c5697a61dfc46b2096963766a5
AnshuPanwar124 <anshupcmpanwar1122@gmail.com>
2025-09-03 23:49:59 +0530

    New Files added

---
+++ Constructor.py
@@ -0,0 +1,24 @@
+class Employee:
+    no_of_leaves = 8   #Class variable is class Property, It is shared by all instances of that class.
+
+    def __init__(self, name, salary, role): #Constructor 
+        self.name = name
+        self.salary = salary
+        self.role = role
+     
+    def printdetails(self):
+        return f"Name is {self.name}. Salary is {self.salary} and role is {self.role}"
+
+harry = Employee("Harry", 455, "Instructor")
+print(harry.salary)
+'''Rohan = Employee()
+
+harry.name = "Harry"
+harry.salary = 455
+harry.role = "Instructor"
+
+Rohan.name = "Rohan"
+Rohan.salary = 4554
+Rohan.role = "Student"
+
+print(Rohan.printdetails())'''
