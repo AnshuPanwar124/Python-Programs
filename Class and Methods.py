8a833733f13a56302b3a72d41985c038503cff12
AnshuPanwar124 <anshupcmpanwar1122@gmail.com>
2025-09-04 23:56:39 +0530

    New Files added

---
+++ Class and Methods.py
@@ -0,0 +1,21 @@
+class Employee:
+    no_of_leaves = 8   #Class variable is class Property, It is shared by all instances of that class.
+
+    def __init__(self, name, salary, role): #Constructor is used for initialize the object ,called automatically when an object is created from a class.
+        self.name = name
+        self.salary = salary
+        self.role = role
+     
+    def printdetails(self):
+        return f"Name is {self.name}. Salary is {self.salary} and role is {self.role}"
+    
+    @classmethod             # It is bound to the class itself, rather than to an instance of the class.
+    def change_leaves(cls, newleaves):
+        cls.no_of_leaves=newleaves
+
+rohan = Employee("Rohan", 255, "Instructor")
+harry = Employee("Harry", 455, "Instructor")
+harry.change_leaves(34)
+
+#Employee.no_of_leaves = 9
+print(harry.no_of_leaves)
