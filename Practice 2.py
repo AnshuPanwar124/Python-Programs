cb70d905f892145888e1fba2e4433b51b959f5c3
AnshuPanwar124 <anshupcmpanwar1122@gmail.com>
2025-10-08 00:05:18 +0530

    New Files added

---
+++ Practice 2.py
@@ -0,0 +1,16 @@
+try:
+    
+    apples = int(input("Enter the number of apples\n"))
+    mn = int(input("Enter the minimum number to check\n"))
+    mx = int(input("Enter the maximum number to check\n"))
+
+except ValueError:
+    print("Enter integers only")
+    exit()
+
+for i in range(mn, mx+1):
+    if apples%i==0:
+      print(f"{i} is a divisor of {apples}")
+      
+    else:
+        print(f"{i} is not a divisor of {apples}")
